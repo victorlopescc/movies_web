a5a7ca271831c91ccdb4e5da6eea2d823e2f576f
victorlopescc <victorlopes.cc@gmail.com>
2023-12-21 00:12:43 -0300

    feat: SingIn page

--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -1,5 +1,47 @@
 import styled from 'styled-components';
+import backgroundImg from '../../assets/background.png';
 
 export const Container = styled.div`
+    height: 100vh;
 
+    display: flex;
+    align-items: stretch;
+`;
+
+export const Form = styled.form`
+    padding: 0 136px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: start;
+
+    text-align: center;
+
+    > h1 {
+        font-size: 48px;
+        color: ${({ theme }) => theme.COLORS.PINK};
+    }
+
+    > p {
+        font-size: 14px;
+        color: ${({ theme }) => theme.COLORS.GRAY_100};
+    }
+
+    > h2 {
+        font-size: 24px;
+        margin: 48px 0;
+    }
+
+    > a {
+        margin-top: 42px;
+        color: ${({ theme }) => theme.COLORS.PINK};
+        align-self: center;
+    }
+`;
+
+export const Background = styled.div`
+    flex: 1;
+    background: url(${backgroundImg}) no-repeat center center;
+    background-size: cover;
 `;
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,15 +1,13 @@
 export default {
     COLORS: {
-        BACKGROUND_900: "#232129",
+        BACKGROUND_900: "#1C1B1E",
         BACKGROUND_800: "#312E38",
-        BACKGROUND_700: "#3E3B47",
+        BACKGROUND_700: "#262529",
 
         WHITE: "#F4EDE8",
-        ORANGE: "#FF9000",
+        GRAY_100: "#CAC4CF",
+        GRAY_300: "#948F99",
 
-        GRAY_100: "#999591",
-        GRAY_300: "#666360",
-
-        RED: "#FF002E"
+        PINK: "#FF859B",
     }
 }
