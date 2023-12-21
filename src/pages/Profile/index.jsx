ce797e855e4fa09b9e5018a4f9101178abc4c349
victorlopescc <victorlopes.cc@gmail.com>
2023-12-21 13:05:33 -0300

    feat: Profile page

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,5 +1,29 @@
-import { Container } from './styles';
+import { Container, Form, Avatar } from './styles';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi';
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
 
 export function Profile() {
-
+    return (
+        <Container>
+            <header>
+                <Link to="/"><FiArrowLeft /> Voltar</Link>
+            </header>
+            <Form>
+                <Avatar>
+                    <img src="https://github.com/victorlopescc.png" alt="Foto do usuário" />
+                    <label htmlFor="avatar">
+                        <FiCamera />
+                        <input id="avatar" type="file" />
+                    </label>
+                </Avatar>
+                <Input placeholder="Nome" type="text" icon={FiUser} />
+                <Input placeholder="E-mail" type="text" icon={FiMail} />
+                <Input placeholder="Senha atual" type="password" icon={FiLock} />
+                <Input placeholder="Nova senha" type="password" icon={FiLock} />
+                <Button title="Salvar" />
+            </Form>
+        </Container>
+    );
 }
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,6 +3,7 @@ export default {
         BACKGROUND_900: "#1C1B1E",
         BACKGROUND_800: "#312E38",
         BACKGROUND_700: "#262529",
+        BACKGROUND_PINK: "rgba(255, 133, 155, 0.05)",
 
         WHITE: "#F4EDE8",
         GRAY_100: "#CAC4CF",
